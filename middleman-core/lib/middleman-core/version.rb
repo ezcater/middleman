f8ea1f1ef8e1aeca5109d914b3cf6f303732a6ff
Tyler Ewing <tewing10@gmail.com>
2021-03-19 17:08:37 -0500

    Loosen constraints on i18n and padrino-helpers to support rails 6.1

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '4.3.13'.freeze unless const_defined?(:VERSION)
+  VERSION = '4.3.14'.freeze unless const_defined?(:VERSION)
 end
